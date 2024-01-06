4229c2fad878ebfe990615e92eb1f5f7997f70f9
zhao252627575 <zhaoshenzhi1991@163.com>
2024-01-06 20:09:47 +0800

    Initial commit

---
+++ p1.py
@@ -0,0 +1,23 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
+from bs4 import BeautifulSoup
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        title = bs.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+#############main############
+title = getTitle('http://pythonscraping.com/pages/page1.html')
+
+if title == None:
+    print('Title coule not be found')
+else:
+    print(title)
