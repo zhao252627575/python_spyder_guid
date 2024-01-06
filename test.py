a9baae4c87b63b1ced5437a250330788a5cdcf3a
zhao252627575 <zhaoshenzhi1991@163.com>
2024-01-07 00:29:50 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	p2.ipynb

--- test.py
+++ test.py
@@ -7,3 +7,9 @@ bs = BeautifulSoup(html.read(), 'html.parser')
 
 for child in bs.find('table', {'id':'gift'}).children:
     print(child)
+
+
+
+from IPython.display import Image
+Image(filename = 'D:\\file\\PythonFiles\\python_spyder_guid\\png\\re1.png', width=320, height=240)
+Image(filename = 'D:\\file\\PythonFiles\\python_spyder_guid\\png\\re2.png', width=320, height=240)
