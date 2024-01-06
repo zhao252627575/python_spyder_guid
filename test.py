9790a8108a66bfbd1e38f816ec098e9676fd0201
zhao252627575 <zhaoshenzhi1991@163.com>
2024-01-06 21:43:16 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	p2.ipynb

---
+++ test.py
@@ -0,0 +1,9 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen('http://www.pythonscraping.com/pages/warandpeace.html')
+bs = BeautifulSoup(html.read(), 'html.parser')
+
+bs.find_all(id='text')
+
+print(bs)
